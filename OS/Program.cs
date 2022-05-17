fc13ba2362a35428d99fca0432064de6fc8cc4e8
wtmzoo <53226641+wtmzoo@users.noreply.github.com>
2022-05-17 13:52:04 +0300

    Changed start menu

--- OS/Program.cs
+++ OS/Program.cs
@@ -1,28 +1,60 @@
 ﻿using System;
-using System.IO;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Security.Cryptography;
 
 
 namespace OS
-{ 
-
-    class Praktika_3
-    { 
-        public static void Function()
-        { 
-            int a=0, b=3, c=3
-            for ( int i=0; i<noIterations; i++ )
-                a += b*2 + c - i;
-            Console.WriteLine("Hello, World!");
-        }
-
-        void main()
+{
+    class Program
+    {
+        static void Main(string[] args)
         {
-
-
+            Menu();
+            Console.WriteLine("The program has finished execution");
         }
-    
+
+
+        static void Menu()
+        {    
+            for (;;)
+            {
+                Console.WriteLine("1. Run Praktika 1\n2. Run Praktika 2\n3. Run Praktika 3\n4. Run Praktika 4\n5. Run Praktika 5\n6. Run Praktika 6\n7. Clear Console");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("\nPraktika_1 Running...\n");
+                        Praktika_1.Start();
+                        break;
+                    case 2:
+                        Console.WriteLine("\nPraktika_2 Running...\n");
+                        Praktika_2.Start();
+                        break;
+                    case 3:
+                        Console.WriteLine("\nPraktika_3 Running...\n");
+                        Praktika_2.Start();
+                        break;
+                    case 4:
+                        Console.WriteLine("\nPraktika_4 Running...\n");
+                        Praktika_2.Start();
+                        break;
+                    case 5:
+                        Console.WriteLine("\nPraktika_5 Running...\n");
+                        Console.WriteLine("In progress\n");
+                        break;
+                    case 6:
+                        Console.WriteLine("\nPraktika_6 Running...\n");
+                        Console.WriteLine("In progress\n");
+                        break;
+                    case 7:
+                        Console.Clear();
+                        break;
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Selection error. Select another option");
+                        break;
+                }
+            }
+        }
+
     }
+}
